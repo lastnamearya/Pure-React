c238b3e75adc1e9fb9cf5a29f6c7973fb2ddc5a5
lastnamearya <arya.jigyasu6815@gmail.com>
2018-06-06 19:25:34 +0530

    Avatar Component added

--- static-tweet/src/index.js
+++ static-tweet/src/index.js
@@ -5,9 +5,20 @@ import './index.css';
 function Tweet() {
   return (
     <div className="tweet">
+      <Avatar />
       Tweet
     </div>
   )
+}
+
+function Avatar() {
+  return (
+    <img
+      src="https://s.gravatar.com/avatar/413b990ccd2cf5ba69d609fdba4f0302?s=80&r=x"
+      className="avatar"
+      alt="avatar"
+    />
+  );
 }
 
 ReactDOM.render(
