5bbf1101f676f373bc9c3df2e2b13ee861d08f8d
lastnamearya <arya.jigyasu6815@gmail.com>
2018-06-06 16:14:12 +0530

    Replace JSX with React.createElement

--- jsx-exercises/src/index.js
+++ jsx-exercises/src/index.js
@@ -1,36 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-function MyThing() {
-  return (
-    <div className="book">
-
-      <div className="title">
-        The Title
-      </div>
-
-      <div className="author">
-        The Author
-      </div>
-
-      <ul className="stats">
-
-        <li className="rating">
-          5 Stars
-        </li>
-
-        <li className="isbn">
-          12-345678-910
-        </li>
-
-      </ul>
-
-    </div>
+function MyThingJSX() {
+  return React.createElement(
+    "div",
+    { className: "book" },
+    React.createElement(
+      "div",
+      { className: "title" },
+      "The Title"
+    ),
+    React.createElement(
+      "div",
+      { className: "author" },
+      "The Author"
+    ),
+    React.createElement(
+      "ul",
+      { className: "stats" },
+      React.createElement(
+        "li",
+        { className: "rating" },
+        "5 Stars"
+      ),
+      React.createElement(
+        "li",
+        { className: "isbn" },
+        "12-345678-910"
+      )
+    )
   );
 }
 
+// function MyThing() {
+//   return (
+//     <div className="book">
+
+//       <div className="title">
+//         The Title
+//       </div>
+
+//       <div className="author">
+//         The Author
+//       </div>
+
+//       <ul className="stats">
+
+//         <li className="rating">
+//           5 Stars
+//         </li>
+
+//         <li className="isbn">
+//           12-345678-910
+//         </li>
+
+//       </ul>
+
+//     </div>
+//   );
+// }
+
 ReactDOM.render(
-  <MyThing/>,
+  <MyThingJSX/>,
   document.getElementById('root')
 );
