7058cb46df329a8cdc0c66ca8087eff72161c099
lastnamearya <arya.jigyasu6815@gmail.com>
2018-06-06 19:54:06 +0530

    Done with the Static Tweet Component

--- static-tweet/src/index.js
+++ static-tweet/src/index.js
@@ -7,8 +7,14 @@ function Tweet() {
     <div className="tweet">
       <Avatar />
       <div className="content">
-        <NameWithHandle/>
+        <NameWithHandle/> <Time/>
         <Message/>
+        <div className="buttons">
+          <ReplyButton/>
+          <RetweetButton/>
+          <LikeButton/>
+          <MoreOptionsButton/>
+        </div>
       </div>
     </div>
   );
@@ -44,6 +50,26 @@ function NameWithHandle() {
     </span>
   );
 }
+
+const Time = () => (
+  <span className="time">3h ago</span>
+);
+
+const ReplyButton = () => (
+  <i className="fa fa-reply reply-button"/>
+);
+
+const RetweetButton = () => (
+  <i className="fa fa-retweet retweet-button"/>
+);
+
+const LikeButton = () => (
+  <i className="fa fa-heart like-button"/>
+);
+
+const MoreOptionsButton = () => (
+  <i className="fa fa-ellipsis-h more-options-button"/>
+);
 
 ReactDOM.render(
   <Tweet />,
