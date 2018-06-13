e68ce6231bedbbb1d1f4be7d0a708d4b3c5f75c0
lastnamearya <arya.jigyasu6815@gmail.com>
2018-06-13 13:05:38 +0530

    custom PropTypes Validator

--- sample code practice/src/index.js
+++ sample code practice/src/index.js
@@ -2,29 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
-function Comment({ author, message, likes }) {
-  return (
-    <div>
-      <div className="author">{author}</div>
-      <div className="message">{message}</div>
-      <div className="likes">
-        {likes > 0 ? likes : 'No'} likes
-      </div>
-    </div>
-  );
+function customValidator(props, propName, componentName) {
+  // here, propName === "myCustomProp"
+  if (props[propName].length !== 3) {
+    return new Error (
+      'Invalid prop `' + propName + '` supplied to' + ' `' + componentName + '`. Length is not 3.'
+    );
+  }
 }
 
-Comment.propTypes = {
-  message: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  likes: PropTypes.number
+const CustomTest = ({ myCustomProp }) => (
+  <span>{myCustomProp}</span>
+);
+
+CustomTest.propTypes = {
+  myCustomProp: customValidator
 }
 
 ReactDOM.render(
-  <Comment 
-    author="somebody"
-    message="a likeable message"
-    likes={0}
+  <CustomTest
+    myCustomProp="not_three_letters"
   />,
   document.querySelector('#root')
 );
