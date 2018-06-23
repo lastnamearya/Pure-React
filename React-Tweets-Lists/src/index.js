f8ded03071c841801e4542267db5a08fc958ff9d
lastnamearya <arya.jigyasu6815@gmail.com>
2018-06-23 12:54:49 +0530

    Tweet Component is now a separate module

--- React-Tweets-Lists/src/index.js
+++ React-Tweets-Lists/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Tweet from './tweet';
 import './index.css';
-import moment from 'moment';
-import PropTypes from 'prop-types';
 
 function TweetList({ list }) {
   return (
@@ -12,128 +11,6 @@ function TweetList({ list }) {
     </div>
   )
 }
-
-function Tweet({ tweet }) {
-  return (
-    <div className="tweet">
-      <Avatar hash={tweet.gravatar}/>
-      <div className="content">
-        <NameWithHandle author={tweet.author}/>
-        <Time time={tweet.timestamp}/>
-        <Message text={tweet.message}/>
-        <div className="buttons">
-          <ReplyButton/>
-          <RetweetButton count={tweet.retweets}/>
-          <LikeButton count={tweet.likes}/>
-          <MoreOptionsButton/>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function Avatar({ hash }) {
-  var url = `https://www.gravatar.com/userimage/${hash}`;
-  return (
-    <img
-      src={url}
-      className="avatar"
-      alt="avatar"
-    />
-  );
-}
-
-Avatar.propTypes = {
-  hash: PropTypes.string.isRequired
-};
-
-function Message({ text }) {
-  return (
-    <div className="message">
-      {text}
-    </div>
-  );
-}
-
-Message.propTypes = {
-  text: PropTypes.string.isRequired
-};
-
-function NameWithHandle({ author }) {
-  const { name, handle } = author;
-  return (
-    <span className="name-with-handle">
-      <span className="name">
-        {name}
-      </span>
-      <span className="handle">
-        {handle}
-      </span>
-    </span>
-  );
-}
-
-NameWithHandle.propTypes = {
-  author: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    handle: PropTypes.string.isRequired
-  }).isRequired
-};
-
-const Time = ({ time }) => {
-  const timeString = moment(time).fromNow();
-  return (
-    <span className="time">
-      {timeString}
-    </span>
-  );
-};
-
-const ReplyButton = () => (
-  <i className="fa fa-reply reply-button"/>
-);
-
-function getRetweetCount(count) {
-  if(count > 0) {
-    return (
-      <span className="retweet-count">
-        {count}
-      </span>
-    );
-  } else {
-    return null;
-  }
-}
-
-const RetweetButton = ({ count }) => (
-  <span className="retweet-button">
-    <i className="fa fa-retweet"/>
-    {getRetweetCount(count)}
-  </span>
-);
-
-RetweetButton.propTypes = {
-  count: PropTypes.number
-};
-
-const LikeButton = ({ count }) => (
-  <span className="like-button">
-    <i className="fa fa-heart"/>
-    {count > 0 && 
-      <span className="like-count">
-        {count}
-      </span>
-    }
-  </span>
-);
-
-LikeButton.propTypes = {
-  count: PropTypes.number
-};
-
-const MoreOptionsButton = () => (
-  <i className="fa fa-ellipsis-h more-options-button"/>
-);
 
 var testTweet = [{
   message: "Learning React.js Full-time.",
@@ -153,8 +30,8 @@ var testTweet = [{
     handle: "@lastnamearya",
     name: "Jigyasu Arya"
   },
-  likes: 5,
-  retweets: 2,
+  likes: 7,
+  retweets: 1,
   timestamp: "2016-07-30 21:24:37"
 },
 {
@@ -164,8 +41,8 @@ var testTweet = [{
     handle: "@lastnamearya",
     name: "Jigyasu Arya"
   },
-  likes: 5,
-  retweets: 2,
+  likes: 14,
+  retweets: 102,
   timestamp: "2016-07-30 21:24:37"
 },
 {
@@ -175,8 +52,8 @@ var testTweet = [{
     handle: "@lastnamearya",
     name: "Jigyasu Arya"
   },
-  likes: 5,
-  retweets: 2,
+  likes: 1,
+  retweets: 0,
   timestamp: "2016-07-30 21:24:37"
 }];
 
