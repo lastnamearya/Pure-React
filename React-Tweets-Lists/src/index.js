cad6a05e35e6a3bc331df768a370d803b5a4ffa3
lastnamearya <arya.jigyasu6815@gmail.com>
2018-06-23 12:29:20 +0530

    Rendering multiple tweets

--- React-Tweets-Lists/src/index.js
+++ React-Tweets-Lists/src/index.js
@@ -136,11 +136,44 @@ const MoreOptionsButton = () => (
 );
 
 var testTweet = [{
+  message: "Learning React.js Full-time.",
+  gravatar: "73469566/d027c3e54488c6a3bd3f297f51977fc2.png",
+  author: {
+    handle: "@lastnamearya",
+    name: "Jigyasu Arya"
+  },
+  likes: 5,
+  retweets: 2,
+  timestamp: "2016-07-30 21:24:37"
+},
+{
+  message: "Something about dogs.",
+  gravatar: "73469566/d027c3e54488c6a3bd3f297f51977fc2.png",
+  author: {
+    handle: "@lastnamearya",
+    name: "Jigyasu Arya"
+  },
+  likes: 5,
+  retweets: 2,
+  timestamp: "2016-07-30 21:24:37"
+},
+{
+  message: "Something not about cats.",
+  gravatar: "73469566/d027c3e54488c6a3bd3f297f51977fc2.png",
+  author: {
+    handle: "@lastnamearya",
+    name: "Jigyasu Arya"
+  },
+  likes: 5,
+  retweets: 2,
+  timestamp: "2016-07-30 21:24:37"
+},
+{
   message: "Something about cats.",
   gravatar: "73469566/d027c3e54488c6a3bd3f297f51977fc2.png",
   author: {
-    handle: "@catperson",
-    name: "IAMA Cat Person"
+    handle: "@lastnamearya",
+    name: "Jigyasu Arya"
   },
   likes: 5,
   retweets: 2,
