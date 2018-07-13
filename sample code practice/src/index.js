9689ccd1cf1eec27b41b84b5fd30914be3fad7c8
lastnamearya <arya.jigyasu6815@gmail.com>
2018-07-13 16:49:03 +0530

    All LifeCycle Components in console

--- sample code practice/src/index.js
+++ sample code practice/src/index.js
@@ -1,51 +1,104 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Content extends React.Component {
-  constructor(props) {
-    super(props);
+class ErrorCatcher extends React.Component {
+  state = { error: null };
 
-    console.log("constructor");
-  }
-
-  componentDidMount() {
-    console.log("componentDidMount");
-  }
-
-  componentWillUnmount() {
-    console.log("componentWillUnmount");
+  componentDidCatchError(error, info) {
+    console.log("[componentDidCatch]", error);
+    this.setState({ error: info.componentStack });
   }
 
   render() {
-    console.log("render");
-    return <div className="content">Hello, World!</div>;
+    if (this.state.error) {
+      return <div>An error occured: {this.state.error}</div>;
+    }
+    return this.props.children;
   }
 }
 
-class App extends React.Component {
+class LifeCycleDemo extends React.Component {
+  // Initialize state first
+  // (happens before constructor)
+  state = { counter: 0 };
+
+  // The first method called after initializing state
   constructor(props) {
     super(props);
-
-    this.state = {
-      expanded: false
-    };
+    console.log("[constructor]");
+    console.log(" State already set:", this.state);
   }
 
-  toggleComponent() {
-    this.setState({ expanded: !this.state.expanded });
+  // Called after initial render is done
+  componentDidMount() {
+    console.log("[componentDidMount]", "Mounted");
   }
+
+  // ** Don't forget to make this `static` ! **
+  // Called before initial render, any time new props are received
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log("[getDerivedStateFromProps]");
+    console.log(" Next props:", nextProps);
+    console.log(" Prev State:", prevState);
+    return null;
+  }
+
+  // Called before each render. Return false to prevent rendering.
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[shouldComponentUpdate]", "Deciding to update");
+    return true;
+  }
+
+  // Called after render() but before updating the DOM
+  // A good time to make calculations based on old DOM nodes.
+  // The value returned here is passed into componentDidUpdate
+  getSnapshotBeforeUpdate(nextProps, nextState) {
+    console.log("[getSnapshotBeforeUpdate]", "About to update...");
+    return `Time is ${Date.now()}`;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("[componentDidUpdate]", "Updated.");
+    console.log(" snapshot:", snapshot);
+  }
+
+  componentWillUnmount() {
+    console.log("[componentWillUnmount]", "Goodbye cruel world.");
+  }
+
+  handleClick = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
+
+  causeErrorNextRender = () => {
+    // Set a flag to cause an error on the next render
+    // This will cause componentDidCatch to run in the parent
+    this.setState({
+      causeError: true
+    });
+  };
 
   render() {
+    console.log("[render]");
+    if (this.state.causeError) {
+      throw new Error("oh no!!");
+    }
+
     return (
       <div>
-        <button type="button" onClick={this.toggleComponent.bind(this)}>
-          Toggle
-          {this.state.expanded ? <Content /> : <div />}
-        </button>
+        <span>Counter: {this.state.counter}</span>
+        <button onClick={this.handleClick}>Click to increment</button>
+        <button onClick={this.causeErrorNextRender}>Throw an error</button>
       </div>
     );
   }
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <ErrorCatcher>
+    <LifeCycleDemo />
+  </ErrorCatcher>,
+  document.getElementById("root")
+);
