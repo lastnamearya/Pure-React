0f145efb96019cecc7608adf56481cbf2dab3e53
lastnamearya <arya.jigyasu6815@gmail.com>
2018-07-13 22:30:53 +0530

    performance ~ arrow now passed to a new child component

--- shopping-site/src/Nav.js
+++ shopping-site/src/Nav.js
@@ -4,13 +4,27 @@ const Nav = ({ activeTab, onTabChange }) => (
   <nav className="App-nav">
     <ul>
       <li className={`App-nav-item ${activeTab === 0 && "selected"}`}>
-        <a onClick={() => onTabChange(0)}>Items</a>
+        <NavLink index={0} onClick={onTabChange}>
+          Items
+        </NavLink>
       </li>
       <li className={`App-nav-item ${activeTab === 1 && "selected"}`}>
-        <a onClick={() => onTabChange(1)}>Cart</a>
+        <NavLink index={1} onClick={onTabChange}>
+          Cart
+        </NavLink>
       </li>
     </ul>
   </nav>
 );
+
+class NavLink extends React.Component {
+  handleClick = () => {
+    this.props.onClick(this.props.index);
+  };
+
+  render() {
+    return <a onClick={this.handleClick}>{this.props.children}</a>;
+  }
+}
 
 export default Nav;
